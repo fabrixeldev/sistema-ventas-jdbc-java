1bd7f7c0f5ed3efb19bad76b1a5bdde59fe7ce3b
Fabricio Leonardini <jfabrixel@gmail.com>
2025-11-25 22:04:23 -0400

    Modificacion del DAO Bitacora utilizando JDBC

--- src/dao/BitacoraDao.java
+++ src/dao/BitacoraDao.java
@@ -1,79 +1,98 @@
 package dao;
 
+
 import modelo.Bitacora;
+import modelo.Conexion;
 import modelo.Usuario;
 
+import java.sql.*;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Optional;
+import java.util.List;
 
 public class BitacoraDao {
-    private Usuario usuario;
 
-    private ArrayList<Bitacora> listaBitacora = new ArrayList<>();
+    public void agregarBitacora(Bitacora b){
+        String sql = "INSERT INTO bitacora (bitacora_codigo, bitacora_fecha, bitacora_horaInicio, bitacora_horaFin, bitacora_tipoUsuario, bitacora_ano, bitacora_id_usuario) VALUES (?,?,?,?,?,?,?)";
 
-    public void agregarBitacora(int bitaId, String bitaCod, Date bitaFecha, Date bitaHoraInicio, Date bitaHoraFin, String bitaTipoUsu, Date bitaAño, Usuario usuario){
-        Bitacora bitacoraNueva = new Bitacora(bitaId, bitaCod, bitaFecha, bitaHoraInicio, bitaHoraFin, bitaTipoUsu, bitaAño, usuario);
-        listaBitacora.add(bitacoraNueva);
-        System.out.println("Se registro la bitacora: " + bitacoraNueva.getBitaCod());
-    }
-
-    public void listarBitacora (){
-        if (listaBitacora.size() != 0){
-            System.out.println(listaBitacora);
-        }else{
-            System.out.println("No existen datos para mostrar");
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+            ps.setString(1, b.getBitaCod());
+            ps.setDate(2, java.sql.Date.valueOf(b.getBitaFecha()));
+            ps.setTime(3, java.sql.Time.valueOf(b.getBitaHoraInicio()));
+            ps.setTime(4, java.sql.Time.valueOf(b.getBitaHoraFin()));
+            ps.setString(5, b.getBitaTipoUsu());
+            ps.setDate(6, Date.valueOf(b.getBitaAno()));
+            ps.setInt(7, b.getUsuario().getUsuId());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public void editarBitacora(int bitaId, String bitaCod, Date bitaFecha, Date bitaHoraInicio, Date bitaHoraFin, String bitaTipoUsu, Date bitaAño, Usuario usuario){
-        for (Bitacora bitacora: listaBitacora){
-            if (bitacora.getBitaId() == bitaId){
-                if (!bitaCod.equals("")){
-                    bitacora.setBitaCod(bitaCod);
-                }
-                if (bitaFecha != null){
-                    bitacora.setBitaFecha(bitaFecha);
-                }
-                if (bitaHoraInicio != null){
-                    bitacora.setBitaHoraInicio(bitaHoraInicio);
-                }
-                if (bitaHoraFin != null){
-                    bitacora.setBitaHoraFin(bitaHoraFin);
-                }
-                if (!bitaTipoUsu.equals("")){
-                    bitacora.setBitaTipoUsu(bitaTipoUsu);
-                }
-                if (usuario != null){
-                    bitacora.setUsuario(usuario);
+    public List<Bitacora> listarBitacora (){
+        List<Bitacora> lista = new ArrayList<>();
+        String sql = "SELECT b.*, u.usuario_codigo FROM bitacora b INNER JOIN usuario u ON bitacora_id_usuario = usuario_id;";
+
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql); ResultSet rs = ps.executeQuery()){
+            while (rs.next()){
+                Bitacora b = new Bitacora();
+                Usuario u = new Usuario();
+                b.setBitaId(rs.getInt("bitacora_id"));
+                b.setBitaCod(rs.getString("bitacora_codigo"));
+                b.setBitaFecha(rs.getDate("bitacora_fecha").toLocalDate());
+                b.setBitaHoraInicio(rs.getTime("bitacora_horaInicio").toLocalTime());
+                b.setBitaHoraFin(rs.getTime("bitacora_horaFin").toLocalTime());
+                b.setBitaTipoUsu(rs.getString("bitacora_tipoUsuario"));
+                b.setBitaAno(rs.getDate("bitacora_ano").toLocalDate());
+                u.setUsuCodigo(rs.getString("usuario_codigo"));
+
+                b.setUsuario(u);
+                lista.add(b);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return lista;
+    }
+
+    public void eliminarBitacora (Bitacora b){
+        String sql = "DELETE FROM bitacora WHERE bitacora_id = ?";
+
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+            ps.setInt(1, b.getBitaId());
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Bitacora> buscarBitacora(String bitaCod){
+        String sql = "SELECT b.*, u.usuario_codigo FROM bitacora b INNER JOIN usuario u ON bitacora_id_usuario = usuario_id WHERE bitacora_codigo = ?";
+        List<Bitacora> lista = new ArrayList<>();
+
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+            ps.setString(1, bitaCod);
+
+            try (ResultSet rs = ps.executeQuery()){
+                while (rs.next()){
+                    Bitacora b = new Bitacora();
+                    Usuario u = new Usuario();
+                    b.setBitaId(rs.getInt("bitacora_id"));
+                    b.setBitaCod(rs.getString("bitacora_codigo"));
+                    b.setBitaFecha(rs.getDate("bitacora_fecha").toLocalDate());
+                    b.setBitaHoraInicio(rs.getTime("bitacora_horaInicio").toLocalTime());
+                    b.setBitaHoraFin(rs.getTime("bitacora_horaFin").toLocalTime());
+                    b.setBitaTipoUsu(rs.getString("bitacora_tipoUsuario"));
+                    b.setBitaAno(rs.getDate("bitacora_ano").toLocalDate());
+                    u.setUsuCodigo(rs.getString("usuario_codigo"));
+                    b.setUsuario(u);
+                    lista.add(b);
                 }
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-    }
-
-    public void eliminarBitacora (int bitaId){
-        Bitacora encontrado = null;
-        for (Bitacora b : listaBitacora){
-            if (b.getBitaId() == bitaId){
-                encontrado = b;
-            }
-        }
-
-        if (encontrado != null){
-            listaBitacora.remove(encontrado);
-            System.out.println("Se elimino " + encontrado.getBitaCod());
-        }
-    }
-
-    public void buscarBitacora(int bitaId){
-        Optional<Bitacora> busqueda = listaBitacora.stream()
-                .filter(s -> s.getBitaId() == bitaId)
-                .findFirst();
-
-        if (busqueda.isPresent()){
-            System.out.println("Bitacora encontrada: " + toString());
-        }else {
-            System.out.println("No es encontro la bitacora: " + bitaId);
-        }
+        return lista;
     }
 }
