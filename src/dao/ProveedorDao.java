8878c03eec88fa80abba51df368b38f74a26cb93
Fabricio Leonardini <jfabrixel@gmail.com>
2025-11-26 22:13:24 -0400

    Modificando el DAO Proveedor utilizando JDBC

--- src/dao/ProveedorDao.java
+++ src/dao/ProveedorDao.java
@@ -1,89 +1,123 @@
 package dao;
 
+import modelo.Conexion;
 import modelo.Proveedor;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Optional;
+import java.util.List;
 
 public class ProveedorDao {
-    private ArrayList<Proveedor> listaProveedor = new ArrayList<>();
 
-    public void agregarProveedor (int provId, String provCodigo, String provNombre, String provTipoDoc, String provNumDoc, int provCelular, String provDireccion, String provCorrElectr){
-        Proveedor proveedorNuevo = new Proveedor(provId, provCodigo, provNombre, provTipoDoc, provNumDoc, provCelular, provDireccion, provCorrElectr);
-        listaProveedor.add(proveedorNuevo);
-        System.out.println("Se registro el Proveedor; " + proveedorNuevo.getProvCodigo());
-    }
-
-    public void listarProveedor (){
-        if (listaProveedor.size() != 0){
-            System.out.println(listaProveedor);
-        }else{
-            System.out.println("No existen datos para mostrar");
+    public void agregarProveedor (Proveedor p){
+        String sql = "INSERT INTO proveedor (proveedor_codigo, proveedor_nombre, proveedor_tipoDocumento, proveedor_numDocumento, proveedor_celular, proveedor_direccion, proveedor_correoElectronico) VALUES (?,?,?,?,?,?,?)";
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+            ps.setString(1, p.getProvCodigo());
+            ps.setString(2, p.getProvNombre());
+            ps.setString(3, p.getProvTipDoc());
+            ps.setString(4, p.getProvNumDoc());
+            ps.setInt(5, p.getProvCelular());
+            ps.setString(6, p.getProvDireccion());
+            ps.setString(7, p.getProvCorrElectr());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public void editarProveedor(int provId, String provCodigo, String provNombre, String provTipoDoc, String provNumDoc, int provCelular, String provDireccion, String provCorrElectr){
-        for (Proveedor proveedor : listaProveedor){
-            if (proveedor.getProvId() == provId){
-                if (!provCodigo.equals("")){
-                    proveedor.setProvCodigo(provCodigo);
-                }
-                if (!provNombre.equals("")){
-                    proveedor.setProvNombre(provNombre);
-                }
-                if (!provTipoDoc.equals("")){
-                    proveedor.setProvTipDoc(provTipoDoc);
-                }
-                if (!provNumDoc.equals("")){
-                    proveedor.setProvNumDoc(provNumDoc);
-                }
-                if (provCelular != 0){
-                    proveedor.setProvCelular(provCelular);
-                }
-                if (!provDireccion.equals("")){
-                    proveedor.setProvDireccion(provDireccion);
-                }
-                if (!provCorrElectr.equals("")){
-                    proveedor.setProvCorrElectr(provCorrElectr);
-                }
+    public List<Proveedor> listarProveedor (){
+        String sql = "SELECT * FROM proveedor";
+        List<Proveedor> lista = new ArrayList<>();
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql); ResultSet rs = ps.executeQuery()){
+            while (rs.next()){
+                Proveedor p = new Proveedor();
+                p.setProvId(rs.getInt("proveedor_id"));
+                p.setProvNombre(rs.getString("proveedor_codigo"));
+                p.setProvTipDoc(rs.getString("proveedor_tipoDocumento"));
+                p.setProvNumDoc(rs.getString("proveedor_numDocumento"));
+                p.setProvCelular(rs.getInt("proveedor_celular"));
+                p.setProvDireccion(rs.getString("proveedor_direccion"));
+                p.setProvCorrElectr(rs.getString("proveedor_correoElectronico"));
+
+                lista.add(p);
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return lista;
+    }
+
+    public void editarProveedor(Proveedor p){
+        String sql = "UPDATE proveedor SET proveedor_codigo = ?, proveedor_nombre = ?, proveedor_tipoDocumento = ?, proveedor_numDocumento = ?, proveedor_celular = ?, proveedor_direccion = ?, proveedor_correoElectronico = ?";
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+            ps.setString(1, p.getProvCodigo());
+            ps.setString(2, p.getProvNombre());
+            ps.setString(3, p.getProvTipDoc());
+            ps.setString(4, p.getProvNumDoc());
+            ps.setInt(5, p.getProvCelular());
+            ps.setString(6, p.getProvDireccion());
+            ps.setString(7, p.getProvCorrElectr());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public void eliminarProveedor(int provId){
-        Proveedor encontrado = null;
-        for (Proveedor prov : listaProveedor){
-            if (prov.getProvId() == provId){
-                encontrado = prov;
-            }
-        }
-
-        if (encontrado != null){
-            listaProveedor.remove(encontrado);
-            System.out.println("Se elimino " + provId);
-        }else {
-            System.out.println("No se pudo eliminar: " + provId);
+        String sql = "DELETE FROM proveedor WHERE proveedor_id = ?";
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+            ps.setInt(1, provId);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public void buscarProveedor(String provNombre){
-        Optional<Proveedor> busqueda = listaProveedor.stream()
-                .filter(s -> s.getProvNombre().equals(provNombre))
-                .findFirst();
-
-        if (busqueda.isPresent()){
-            System.out.println("Proveedor encontrado: " +toString());
-        }else{
-            System.out.println("No se encuentra coincidencias con: " + provNombre);
+    public List<Proveedor> buscarProveedor(String provNombre){
+        String sql = "SELECT * FROM proveedor WHERE proveedor_nombre = ?";
+        List<Proveedor> lista = new ArrayList<>();
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+            ps.setString(1, provNombre);
+            try (ResultSet rs = ps.executeQuery()){
+                while (rs.next()){
+                    Proveedor p = new Proveedor();
+                    p.setProvId(rs.getInt("proveedor_id"));
+                    p.setProvNombre(rs.getString("proveedor_codigo"));
+                    p.setProvTipDoc(rs.getString("proveedor_tipoDocumento"));
+                    p.setProvNumDoc(rs.getString("proveedor_numDocumento"));
+                    p.setProvCelular(rs.getInt("proveedor_celular"));
+                    p.setProvDireccion(rs.getString("proveedor_direccion"));
+                    p.setProvCorrElectr(rs.getString("proveedor_correoElectronico"));
+                    lista.add(p);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+        return lista;
     }
 
     public Proveedor obtenerProveedorPorId(int provId){
-        for (Proveedor prov: listaProveedor){
-            if (prov.getProvId() == provId){
-                return prov;
+        String sql = "SELECT * FROM proveedor WHERE proveedor_id = ?";
+        Proveedor proveedor = null;
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+            ps.setInt(1, provId);
+            try (ResultSet rs = ps.executeQuery()){
+                proveedor = new Proveedor();
+                proveedor.setProvId(rs.getInt("proveedor_id"));
+                proveedor.setProvNombre(rs.getString("proveedor_codigo"));
+                proveedor.setProvTipDoc(rs.getString("proveedor_tipoDocumento"));
+                proveedor.setProvNumDoc(rs.getString("proveedor_numDocumento"));
+                proveedor.setProvCelular(rs.getInt("proveedor_celular"));
+                proveedor.setProvDireccion(rs.getString("proveedor_direccion"));
+                proveedor.setProvCorrElectr(rs.getString("proveedor_correoElectronico"));
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-        return null;
+        return proveedor;
     }
 }
