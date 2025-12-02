63f5a05e741db6e11b3d56e8f6a4f9780f1b0a29
Fabricio Leonardini <jfabrixel@gmail.com>
2025-12-01 23:46:47 -0400

    Modificacion del DAO Compra utilizando JDBC

--- src/dao/CompraDao.java
+++ src/dao/CompraDao.java
@@ -1,95 +1,140 @@
 package dao;
 
 import modelo.Compra;
+import modelo.Conexion;
 import modelo.Proveedor;
 import modelo.Usuario;
 
+import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Optional;
+import java.util.List;
 
 public class CompraDao {
-    private Usuario usuario;
-    private Proveedor proveedor;
 
-    private ArrayList<Compra> listaCompra = new ArrayList<>();
-
-    public void agregarCompra(int compraId, String compraCodigo, String compraNumComp, Date compraFecha, double compraTotal, boolean compraEstado, Usuario usuario, Proveedor proveedor){
-        Compra compraNueva = new Compra(compraId, compraCodigo, compraNumComp, compraFecha, compraTotal, compraEstado, usuario, proveedor);
-        listaCompra.add(compraNueva);
-        System.out.println("Se registro la Compra: " + compraNueva.getCompraCodigo());
-    }
-
-    public void listarCompra (){
-        if (listaCompra.size() != 0){
-            System.out.println(listaCompra);
-        }else{
-            System.out.println("No existen registros para mostrar");
+    public void agregarCompra(Compra c){
+        String sql = "INSERT INTO compra (compra_codigo, compra_numComprobante, compra_fecha, compra_total , compra_estado, compra_id_usuario, compra_id_proveedor) VALUES (?,?,?,?,?,?,?)";
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+            ps.setString(1, c.getCompraCodigo());
+            ps.setString(2, c.getCompraNumComp());
+            ps.setTimestamp(3, Timestamp.valueOf(c.getCompraFecha()));
+            ps.setDouble(4, c.getCompraTotal());
+            ps.setBoolean(5, c.isCompraEstado());
+            ps.setInt(6, c.getUsuario().getUsuId());
+            ps.setInt(7, c.getProveedor().getProvId());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public void editarCompra(int compraId, String compraCodigo, String compraNumComp, Date compraFecha, double compraTotal, boolean compraEstado, Usuario usuario, Proveedor proveedor){
-        for (Compra compra: listaCompra){
-            if (compra.getCompraId() == compraId){
-                if (!compraCodigo.equals("")){
-                    compra.setCompraCodigo(compraCodigo);
-                }
-                if (!compraNumComp.equals("")){
-                    compra.setCompraNumComp(compraNumComp);
-                }
-                if (compraFecha != null){
-                    compra.setCompraFecha(compraFecha);
-                }
-                if (compraTotal != 0){
-                    compra.setCompraTotal(compraTotal);
-                }
-                if ((compraEstado != true) || (compraEstado != false)){
-                    compra.setCompraEstado(compraEstado);
-                }
-                if (usuario != null){
-                    compra.setUsuario(usuario);
-                }
-                if (proveedor != null){
-                    compra.setProveedor(proveedor);
-                }
+    public List<Compra> listarCompra (){
+        List<Compra> lista = new ArrayList<>();
+        String sql = "SELECT c.*, u.usuario_id, p.proveedor_id FROM compra c INNER JOIN usuario u ON compra_id_usuario = usuario_id INNER JOIN proveedor p ON compra_id_proveedor = proveedor_id";
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql); ResultSet rs = ps.executeQuery()) {
+            while (rs.next()){
+                Compra c = new Compra();
+                Usuario u = new Usuario();
+                Proveedor p = new Proveedor();
+                c.setCompraId(rs.getInt("compra_id"));
+                c.setCompraCodigo(rs.getString("compra_codigo"));
+                c.setCompraNumComp(rs.getString("compra_numComprobante"));
+                c.setCompraFecha(rs.getTimestamp("compra_fecha").toLocalDateTime());
+                c.setCompraTotal(rs.getDouble("compra_total"));
+                c.setCompraEstado(rs.getBoolean("compra_estado"));
+                u.setUsuId(rs.getInt("usuario_id"));
+                p.setProvId(rs.getInt("proveedor_id"));
+                c.setUsuario(u);
+                c.setProveedor(p);
+                lista.add(c);
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return lista;
+    }
+
+    public void editarCompra(Compra c){
+        String sql = "UPDATE compra SET compra_codigo = ?, compra_numComprobante = ?, compra_fecha = ?, compra_total = ?, compra_estado = ?, compra_id_usuario = ?, compra_id_proveedor = ? WHERE compra_id = ?";
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+            ps.setString(1, c.getCompraCodigo());
+            ps.setString(2, c.getCompraNumComp());
+            ps.setTimestamp(3, Timestamp.valueOf(c.getCompraFecha()));
+            ps.setDouble(4, c.getCompraTotal());
+            ps.setBoolean(5, c.isCompraEstado());
+            ps.setInt(6, c.getUsuario().getUsuId());
+            ps.setInt(7, c.getProveedor().getProvId());
+            ps.setInt(8, c.getCompraId());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public void eliminarCompra(int compraId){
-        Compra encontrado = null;
-        for (Compra comp: listaCompra){
-            if (comp.getCompraId() == compraId){
-                encontrado = comp;
-            }
-        }
-
-        if (encontrado != null){
-            listaCompra.remove(encontrado);
-            System.out.println("Se elimino " + compraId);
-        }else {
-            System.out.println("No se pudo eliminar: " + compraId);
+        String sql = "DELETE FROM compra WHERE compra_id = ?";
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+            ps.setInt(1, compraId);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public void buscarCompra(String compraNumComp){
-        Optional<Compra> busqueda = listaCompra.stream()
-                .filter(s -> s.getCompraNumComp().equals(compraNumComp))
-                .findFirst();
-
-        if (busqueda.isPresent()){
-            System.out.println("Compra encontrada: " + toString());
-        }else {
-            System.out.println("No se encontro coincidencias con: " + compraNumComp);
+    public List<Compra> buscarCompra(String compraCodigo){
+        List<Compra> lista = new ArrayList<>();
+        String sql = "SELECT c.*, u.usuario_id, p.proveedor_id FROM compra c INNER JOIN usuario u ON compra_id_usuario = usuario_id INNER JOIN proveedor p ON compra_id_proveedor = proveedor_id WHERE compra_codigo = ?";
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+            ps.setString(1, compraCodigo);
+            try (ResultSet rs = ps.executeQuery()){
+                while (rs.next()){
+                    Compra c = new Compra();
+                    Usuario u = new Usuario();
+                    Proveedor p = new Proveedor();
+                    c.setCompraId(rs.getInt("compra_id"));
+                    c.setCompraCodigo(rs.getString("compra_codigo"));
+                    c.setCompraNumComp(rs.getString("compra_numComprobante"));
+                    c.setCompraFecha(rs.getTimestamp("compra_fecha").toLocalDateTime());
+                    c.setCompraTotal(rs.getDouble("compra_total"));
+                    c.setCompraEstado(rs.getBoolean("compra_estado"));
+                    u.setUsuId(rs.getInt("usuario_id"));
+                    p.setProvId(rs.getInt("proveedor_id"));
+                    c.setUsuario(u);
+                    c.setProveedor(p);
+                    lista.add(c);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+        return lista;
     }
 
     public Compra obtenerCompraPorId(int compraId){
-        for (Compra compra: listaCompra){
-            if (compra.getCompraId() == compraId){
-                return compra;
+        String sql = "SELECT c.*, u.usuario_id, p.proveedor_id FROM compra c INNER JOIN usuario u ON compra_id_usuario = usuario_id INNER JOIN proveedor p ON compra_id_proveedor = proveedor_id WHERE compra_id = ?";
+        Compra compra = null;
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+            ps.setInt(1, compraId);
+            try (ResultSet rs = ps.executeQuery()){
+                while (rs.next()){
+                    compra = new Compra();
+                    Usuario u = new Usuario();
+                    Proveedor p = new Proveedor();
+                    compra.setCompraId(rs.getInt("compra_id"));
+                    compra.setCompraCodigo(rs.getString("compra_codigo"));
+                    compra.setCompraNumComp(rs.getString("compra_numComprobante"));
+                    compra.setCompraFecha(rs.getTimestamp("compra_fecha").toLocalDateTime());
+                    compra.setCompraTotal(rs.getDouble("compra_total"));
+                    compra.setCompraEstado(rs.getBoolean("compra_estado"));
+                    u.setUsuId(rs.getInt("usuario_id"));
+                    p.setProvId(rs.getInt("proveedor_id"));
+                    compra.setUsuario(u);
+                    compra.setProveedor(p);
+                }
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-        return null;
+        return compra;
     }
 }
