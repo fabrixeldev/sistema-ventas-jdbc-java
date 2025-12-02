515032aa2d4fc3dad8c49c06968729b61577992b
Fabricio Leonardini <jfabrixel@gmail.com>
2025-12-01 23:29:56 -0400

    Modificacion del DAO DetalleVenta utilizando JDBC

--- src/dao/DetalleVentaDao.java
+++ src/dao/DetalleVentaDao.java
@@ -1,79 +1,113 @@
 package dao;
 
+import modelo.Conexion;
 import modelo.DetalleVenta;
 import modelo.Venta;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Optional;
+import java.util.List;
 
 public class DetalleVentaDao {
-    private Venta venta;
 
-    ArrayList<DetalleVenta> listaDetalleVenta = new ArrayList<>();
-
-    public void agregarDetalleVenta(int detVenId, double detVenPrecio, double detVenDescuento, Venta venta){
-        DetalleVenta nuevoDetalleVenta = new DetalleVenta(detVenId, detVenPrecio, detVenDescuento, venta);
-        listaDetalleVenta.add(nuevoDetalleVenta);
-        System.out.println("Se registro el Detalle Venta: " + nuevoDetalleVenta.getDetVenId());
-    }
-
-    public void listarDetalleVenta(){
-        if (listaDetalleVenta.size() != 0){
-            System.out.println(listaDetalleVenta);
-        }else{
-            System.out.println("No existen datos para mostrar");
+    public void agregarDetalleVenta(DetalleVenta dv){
+        String sql = "INSERT INTO detalleVenta (detalleVenta_precioVenta, detalleVenta_descuento, detalleVenta_id_venta) VALUES (?,?,?)";
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+            ps.setDouble(1, dv.getDetVenPrecio());
+            ps.setDouble(2, dv.getDetVenDescuento());
+            ps.setInt(3, dv.getVenta().getVentId());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public void editarDetalleVenta(int detVenId, double detVenPrecio, double detVenDescuento, Venta venta){
-        for (DetalleVenta detalleVenta: listaDetalleVenta){
-            if (detalleVenta.getDetVenId() == detVenId){
-                if (detVenPrecio != 0){
-                    detalleVenta.setDetVenPrecio(detVenPrecio);
-                }
-                if (detVenDescuento != 0){
-                    detalleVenta.setDetVenDescuento(detVenDescuento);
-                }
-                if (venta != null){
-                    detalleVenta.setVenta(venta);
-                }
+    public List<DetalleVenta> listarDetalleVenta(){
+        List<DetalleVenta> lista = new ArrayList<>();
+        String sql = "SELECT dv.*, v.venta_id FROM detalleVenta dv INNER JOIN venta v ON detalleVenta_id_venta = venta_id";
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql); ResultSet rs = ps.executeQuery()){
+            while (rs.next()){
+                DetalleVenta dv = new DetalleVenta();
+                Venta v = new Venta();
+                dv.setDetVenId(rs.getInt("detalleVenta_id"));
+                dv.setDetVenPrecio(rs.getDouble("detalleVenta_precioVenta"));
+                dv.setDetVenDescuento(rs.getDouble("detalleVenta_descuento"));
+                v.setVentId(rs.getInt("venta_id"));
+                dv.setVenta(v);
+                lista.add(dv);
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return lista;
+    }
+
+    public void editarDetalleVenta(DetalleVenta dv){
+        String sql = "UPDATE detalleVenta SET detalleVenta_precioVenta = ?, detalleVenta_descuento = ?, detalleVenta_id_venta = ? WHERE detalleVenta_id = ?";
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+            ps.setDouble(1, dv.getDetVenPrecio());
+            ps.setDouble(2, dv.getDetVenDescuento());
+            ps.setInt(3, dv.getVenta().getVentId());
+            ps.setInt(4, dv.getDetVenId());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public void eliminarDetalleVenta(int detVenId){
-        DetalleVenta encontrado = null;
-        for (DetalleVenta dv : listaDetalleVenta){
-            if (dv.getDetVenId() == detVenId){
-                encontrado = dv;
-            }
-        }
-
-        if (!encontrado.equals(null)){
-            listaDetalleVenta.remove(encontrado);
-            System.out.println("Se elimino: " + detVenId);
-        }else {
-            System.out.println("No se encontro el detalle venta " +detVenId);
+        String sql = "DELETE FROM detalleVenta WHERE detalleVenta_id = ?";
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+            ps.setInt(1, detVenId);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public void buscarDetalleVenta(int detVenId){
-        Optional<DetalleVenta> busqueda = listaDetalleVenta.stream()
-                .filter(s ->  s.getDetVenId() == detVenId)
-                .findFirst();
-
-        busqueda.ifPresentOrElse(
-                s -> System.out.println("Detalle Venta encontrado" + toString()),
-                () -> System.out.println("No se encontro el " + detVenId)
-        );
+    public List<DetalleVenta> buscarDetalleVenta(int detVenId){
+        List<DetalleVenta> lista = new ArrayList<>();
+        String sql = "SELECT dv.*, v.venta_id FROM detalleVenta dv INNER JOIN venta v ON detalleVenta_id_venta = venta_id WHERE detalleVenta_id = ?";
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+            ps.setInt(1, detVenId);
+            try (ResultSet rs = ps.executeQuery()){
+                while (rs.next()){
+                    DetalleVenta dv = new DetalleVenta();
+                    Venta v = new Venta();
+                    dv.setDetVenId(rs.getInt("detalleVenta_id"));
+                    dv.setDetVenPrecio(rs.getDouble("detalleVenta_precioVenta"));
+                    dv.setDetVenDescuento(rs.getDouble("detalleVenta_descuento"));
+                    v.setVentId(rs.getInt("venta_id"));
+                    dv.setVenta(v);
+                    lista.add(dv);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return lista;
     }
 
     public DetalleVenta obtenerDetalleVentaPorId(int detVenId){
-        for (DetalleVenta detVen: listaDetalleVenta){
-            if (detVen.getDetVenId() == detVenId){
-                return detVen;
+        String sql = "SELECT dv.* v.venta_id FROM detalleVenta dv INNER JOIN venta v ON detalleVenta_id_venta = venta_id WHERE detalleVenta_id = ?";
+        DetalleVenta detalleVenta = null;
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql)){
+            ps.setInt(1, detVenId);
+            try (ResultSet rs = ps.executeQuery()){
+                detalleVenta = new DetalleVenta();
+                Venta venta = new Venta();
+                detalleVenta.setDetVenId(rs.getInt("detalleVenta_id"));
+                detalleVenta.setDetVenPrecio(rs.getDouble("detalleVenta_precioVenta"));
+                detalleVenta.setDetVenDescuento(rs.getDouble("detalleVenta_descuento"));
+                venta.setVentId(rs.getInt("venta_id"));
+                detalleVenta.setVenta(venta);
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-        return null;
+        return detalleVenta;
     }
 }
